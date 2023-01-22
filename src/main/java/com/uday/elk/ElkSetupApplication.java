1b4733976bd33d993dd3e91555808fce77023a3a
patiludayk <patiludayk@gmail.com>
2023-01-22 16:15:14 +0000

    Initial commit

---
+++ src/main/java/com/uday/elk/ElkSetupApplication.java
@@ -0,0 +1,13 @@
+package com.uday.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uday/elk/ElkSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
